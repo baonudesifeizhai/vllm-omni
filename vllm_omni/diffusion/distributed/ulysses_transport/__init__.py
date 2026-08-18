8989f238b611941e1a0a808591f1dc21d31669c1
roG0d <baonudesifeizhai@gmail.com>
2026-08-18 07:26:12 +0000

    fix ruff
    Signed-off-by: roG0d <baonudesifeizhai@gmail.com>

--- vllm_omni/diffusion/distributed/ulysses_transport/__init__.py
+++ vllm_omni/diffusion/distributed/ulysses_transport/__init__.py
@@ -1,11 +1,2 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
-
-from vllm_omni.diffusion.distributed.ulysses_transport.base import (
-    UlyssesTransport,
-)
-from vllm_omni.diffusion.distributed.ulysses_transport.factory import (
-    build_ulysses_transport,
-)
-
-__all__ = ["UlyssesTransport", "build_ulysses_transport"]
